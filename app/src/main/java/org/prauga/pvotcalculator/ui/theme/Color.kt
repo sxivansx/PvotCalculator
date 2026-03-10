bb4bf8bdc8bc842b1ef1390223b0e147b24d6e63
Shivansh Pandey <shivanshp987@gmail.com>
2026-03-11 01:42:51 +0530

    Add iOS calculator dark theme
    Black background, dark gray digit buttons, light gray function buttons,
    orange operator buttons. Forces dark-only color scheme.

--- app/src/main/java/org/prauga/pvotcalculator/ui/theme/Color.kt
+++ app/src/main/java/org/prauga/pvotcalculator/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package org.prauga.pvotcalculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// iOS Calculator Colors
+val CalcBlack = Color(0xFF000000)
+val CalcDarkGray = Color(0xFF333333)
+val CalcLightGray = Color(0xFFA5A5A5)
+val CalcOrange = Color(0xFFFF9F0A)
+val CalcWhite = Color(0xFFFFFFFF)
